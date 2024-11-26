77c20b3c4c9f0aa5cab1c46ad6cda42aebc3b293
MErsaJu <erma21ox@student.ju.se>
2024-11-26 12:51:17 +0100

    SNART CHAMPANGE.1.3

--- app/src/main/java/com/example/projecttictactoe/MenuScreen.kt
+++ app/src/main/java/com/example/projecttictactoe/MenuScreen.kt
@@ -3,12 +3,15 @@ package com.example.projecttictactoe
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,6 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -52,7 +56,8 @@ fun MenuScreen(navController: NavController, model: GameModel) {
         games.forEach { (gameId, game) ->
             if ((game.player1Id == model.myPlayerId.value
                                 || game.player2Id == model.myPlayerId.value)
-                                && game.gameState == "player1_turn") {
+                                && game.gameState == "player1_turn"
+                ) {
                 navController.navigate("GameScreen/${gameId}")
             }
         }
@@ -63,106 +68,158 @@ fun MenuScreen(navController: NavController, model: GameModel) {
         playerName = it.name
     }
 
-    Box(
+    Scaffold(
         modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color(0xffc1aeca))
-    ) {
-        Text(
-            text = "Game Requests \n$playerName",
-            color = Color(0xfff5f5f5),
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Center,
-            lineHeight = 1.66.em,
-            style = TextStyle(
-                fontSize = 38.sp,
-                fontWeight = FontWeight.Light,
-                shadow = Shadow(color = Color.DarkGray,
-                    offset = Offset(4f, 6f),
-                    blurRadius = 4f)),
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 24.dp,
-                    y = 147.dp)
-                .requiredWidth(width = 364.dp))
-        IconButton(
-            onClick = { navController.navigate("HomeScreen") },
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 49.dp,
-                    y = 54.dp)
-                .clip(shape = RoundedCornerShape(30.dp))
-                .background(color = Color(0xff65558f))
-        ) {
-            Box(
+            .fillMaxSize(),
+        containerColor = Color(0xffc1aeca),
+        topBar = {
+            TopBar(navController = navController)
+        },
+        content = { innerPadding ->
+            Column(
                 modifier = Modifier
-                    .requiredSize(size = 45.dp)
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.arrow_back),
-                    contentDescription = "icon",
-                    modifier = Modifier
-                        .fillMaxSize())
+                Header(playerName = playerName)
+                Spacer(modifier = Modifier.height(16.dp))
+                GameRequestList(
+                    players = players,
+                    games = games,
+                    model = model,
+                    navController = navController
+                )
             }
         }
-        Box(
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 59.dp,
-                    y = 320.dp)
-                .requiredWidth(width = 294.dp)
-                .requiredHeight(height = 574.dp)
-        ) {
-            LazyColumn (modifier = Modifier) {
-                items(players.entries.toList()) { entry  ->
-                    val player = entry.value
-                    val documentId = entry.key
+    )
+}
 
-                    if (documentId != model.myPlayerId.value) {
-                        ListItem(
-                            headlineContent =  { Text("Player Name: ${player.name}")
-                            },
-                            supportingContent = { Text("Status: ")
-                            },
-                            trailingContent = {
-                                var hasGame = false
-                                games.forEach { (gameId, game) ->
-                                    if (game.player1Id == model.myPlayerId.value
-                                        && game.gameState == "Invite") { Text("Please Wait :')")
-                                        hasGame = true
-                                    } else if (game.player2Id == model.myPlayerId.value
-                                        && game.gameState == "Invite") {
-                                        Button(onClick = {
-                                            model.db.collection("games").document(gameId)
-                                                .update("gameState", "player1_turn")
-                                                .addOnSuccessListener {
-                                                    navController.navigate("GameScreen/${gameId}")
-                                                }
-                                                .addOnFailureListener {
-                                                    Log.e("merry christmas", "error updating game: $gameId")
-                                                }
-                                        }) {
-                                            Text("Accept invite")
-                                        }
-                                        hasGame = true
-                                    }
-                                }
-                                if (!hasGame) {
-                                    Button(onClick = {
-                                        model.db.collection("games")
-                                            .add(Game(gameState = "Invite",
-                                                player1Id = model.myPlayerId.value!!,
-                                                player2Id = documentId
-                                            ))
-                                    }) {
-                                        Text("Challenge")
-                                    }
-                                }
-                            }
+@Composable
+fun TopBar (navController: NavController) {
+    IconButton(
+        onClick = { navController.navigate("HomeScreen") },
+        modifier = Modifier
+            .padding(start = 16.dp, top = 16.dp)
+            .size(50.dp)
+            .clip(shape = RoundedCornerShape(30.dp))
+            .background(color = Color(0xff65558f))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.arrow_back),
+            contentDescription = "Back icon",
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun Header (playerName: String) {
+    Text(
+        text = "Game Requests \n$playerName",
+        color = Color(0xfff5f5f5),
+        fontStyle = FontStyle.Italic,
+        textAlign = TextAlign.Center,
+        lineHeight = 1.66.em,
+        style = TextStyle(
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Light,
+            shadow = Shadow(color = Color.DarkGray,
+                offset = Offset(4f, 6f),
+                blurRadius = 4f
+            )
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp, bottom = 16.dp)
+    )
+}
+
+@Composable
+fun GameRequestList(
+    players: Map<String, Player>,
+    games: Map<String, Game>,
+    model: GameModel,
+    navController: NavController
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(max = 500.dp) // Adjust based on available space
+    ) {
+        items(players.entries.toList()) { entry ->
+            val player = entry.value
+            val documentId = entry.key
+
+            if (documentId != model.myPlayerId.value) {
+                ListItem(
+                    headlineContent = {
+                        Text(text = "Player Name: ${player.name}")
+                    },
+                    supportingContent = {
+                        Text(text = "Status:")
+                    },
+                    trailingContent = {
+                        ManageGameButtons(
+                            games = games,
+                            model = model,
+                            navController = navController,
+                            documentId = documentId
                         )
                     }
-                }
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun ManageGameButtons(
+    games: Map<String, Game>,
+    model: GameModel,
+    navController: NavController,
+    documentId: String
+) {
+    var hasGame = false
+    games.forEach { (gameId, game) ->
+        if (game.player1Id == model.myPlayerId.value
+            && game.player2Id == documentId
+            && game.gameState == "Invite") {
+            Text("Please Wait :')")
+            hasGame = true
+        } else if (game.player2Id == model.myPlayerId.value
+            && game.player1Id == documentId
+            && game.gameState == "Invite") {
+            Button(onClick = {
+                model.db.collection("games").document(gameId)
+                    .update("gameState", "player1_turn")
+                    .addOnSuccessListener {
+                        navController.navigate("GameScreen/$gameId")
+                    }
+                    .addOnFailureListener {
+                        Log.e("Error", "Error updating game: $gameId")
+                    }
+            }) {
+                Text("Accept Invite")
+            }
+            hasGame = true
+        }
+    }
+    if (!hasGame) {
+        Button(onClick = {
+            model.db.collection("games")
+                .add(
+                    Game(
+                        gameState = "Invite",
+                        player1Id = model.myPlayerId.value!!,
+                        player2Id = documentId
+                    )
+                )
+        }) {
+            Text("Challenge")
         }
     }
 }
